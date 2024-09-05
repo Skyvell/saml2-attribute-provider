13150bee35648ce9408b2a7dffefdcb8cfe37b0c
Ted Skyvell <tskyvell@gmail.com>
2024-09-05 15:27:23 +0200

    Remove ds files.

---
+++ saml2-attribute-provider/bin/main/io/curity/identityserver/plugin/ica/data/access/attribute/descriptor/SAMLDataAccessPluginDescriptor.kt
@@ -0,0 +1,14 @@
+package io.curity.identityserver.plugin.ica.data.access.attribute.descriptor
+
+import io.curity.identityserver.plugin.ica.data.access.attribute.SAMLAttributeDataAccessProvider
+import io.curity.identityserver.plugin.ica.data.access.attribute.config.SAMLDataAccessProviderConfiguration
+import se.curity.identityserver.sdk.plugin.descriptor.DataAccessProviderPluginDescriptor
+
+class SAMLDataAccessPluginDescriptor : DataAccessProviderPluginDescriptor<SAMLDataAccessProviderConfiguration>{
+    override fun getPluginImplementationType() = SAMLAttributeDataAccessProvider::class.java.typeName
+
+    override fun getConfigurationType() = SAMLDataAccessProviderConfiguration::class.java
+
+    override fun getAttributeDataAccessProvider() = SAMLAttributeDataAccessProvider::class.java
+
+}
